0199d1f67e3a0d4f7bb78bd2859529240e254155
Jason <jasoncollier@live.com>
2019-04-21 22:04:54 +0200

    Load glucose data

--- public/scripts/dashboard.js
+++ public/scripts/dashboard.js
@@ -5,6 +5,7 @@ initFirebaseAuth();
 loadPatientData();
 
 // Shortcuts to DOM Elements.
+var glucoseList = new Array();
 var signOutElement = document.getElementById('signOut');
 var nameElement = document.getElementById('patientName');
 var messengerRBElement = document.getElementById('messengerRB');
@@ -51,12 +52,30 @@ function getPatientId() {
 }
 
 function loadPatientData() {
-  var patientsRef = firebase.database().ref('patients/' + getPatientId() + '/userData');
-  patientsRef.on("value", function (snapshot) {
-    var userData = snapshot.val();
-    var name = userData.name;
-    nameElement.textContent = name;
-  });
+  // Create the reference to load the patient
+  var patientRef = firebase.database().ref('patients/' + getPatientId());
+
+  // Make sure to remove all previous listeners.
+  patientRef.off();
+
+  // Load previous data and start listening for new data.
+  var setData = function (data) {
+    var gdData = data.val();
+    var glucose = gdData.glucose;
+    glucoseList.push(glucose);
+  };
+  patientRef.child("gdData").on('child_added', setData);
+  patientRef.child("gdData").on('child_changed', setData);
+  
+
+// Load patient name
+var setName = function (data) {
+  var userData = data.val();
+  var name = userData.name;
+  nameElement.textContent = name;
+};
+
+  patientRef.child("userData").on("value", setName);
 }
 
 // Radio button to open messenger
@@ -97,34 +116,30 @@ google.charts.load('current', {
 // Set a callback to run when the Google Visualization API is loaded.
 google.charts.setOnLoadCallback(drawChart);
 
-// Draw chart
-//drawChart();
-
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
 function drawChart() {
 
   // Create the data table.
-  var data = new google.visualization.DataTable();
-  data.addColumn('string', 'Topping');
-  data.addColumn('number', 'Slices');
-  data.addRows([
-    ['Mushrooms', 3],
-    ['Onions', 1],
-    ['Olives', 1],
-    ['Zucchini', 1],
-    ['Pepperoni', 2]
+  var data = google.visualization.arrayToDataTable([
+    ['Day', 'Glucose'],
+    ['5',  5.5],
+    ['10',  5.1],
+    ['15',  4.9],
+    ['20',  6.3],
+    ['25',  7.0],
+    ['30',  5.0]
   ]);
 
   // Set chart options
   var options = {
-    'title': 'How Much Pizza I Ate Last Night',
-    'width': 400,
-    'height': 300
+    title: 'Glucose',
+    curveType: 'function',
+    legend: { position: 'bottom' }
   };
 
   // Instantiate and draw our chart, passing in some options.
-  var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+  var chart = new google.visualization.LineChart(document.getElementById('chart-div'));
   chart.draw(data, options);
 }
