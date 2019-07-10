6652fbb3a857d4a689f852db2f2b02880b60d323
Jason <jasoncollier@live.com>
2019-07-10 15:53:17 +0200

    Added relevant fields to details

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -17,12 +17,20 @@ var idElement = document.getElementById('id');
 var phoneElement = document.getElementById('phone');
 var emailElement = document.getElementById('email');
 var addressElement = document.getElementById('address');
+var dobElement = document.getElementById('dob');
 var heightElement = document.getElementById('height');
+var weightElement = document.getElementById('weight');
+var bmiElement = document.getElementById('bmi');
+var diabetesElement = document.getElementById('diabetes');
 var dueDateElement = document.getElementById('dueDate');
 var glucMinElement = document.getElementById('glucMin');
 var glucMaxElement = document.getElementById('glucMax');
 var weightMinElement = document.getElementById('weightMin');
 var weightMaxElement = document.getElementById('weightMax');
+var actGoalElement = document.getElementById('actGoal');
+var carbsGoalElement = document.getElementById('carbsGoal');
+var medicationElement = document.getElementById('medication');
+var commentElement = document.getElementById('comment');
 
 //Sign out click listener
 signOutElement.addEventListener('click', signOut);
@@ -72,6 +80,9 @@ function loadPatientData() {
     var email = userData.email;
     var address = userData.address;
     var height = userData.height;
+    var weight = userData.prePregWeight;
+    var bmi = weight / ((height / 100) * (height / 100));
+    var diabetes = userData.diabetesType;
     var dueDate = userData.dueDate;
     nameElement.textContent = name;
     fullNameElement.textContent = name;
@@ -79,7 +90,11 @@ function loadPatientData() {
     phoneElement.textContent = phone;
     emailElement.textContent = email;
     addressElement.textContent = address;
+    dobElement.textContent = new Date(id.substring(0,2), id.substring(2,4) - 1, id.substring(4,6));
     heightElement.textContent = height + " cm";
+    weightElement.textContent = weight + " Kg"
+    bmiElement.textContent = bmi + " Kg/m2"
+    diabetesElement.textContent = diabetes;
     dueDateElement.textContent = new Date(dueDate);
   });
 
@@ -90,10 +105,19 @@ function loadPatientData() {
     var glucMax = ranges.glucMax;
     var weightMin = ranges.weightMin;
     var weightMax = ranges.weightMax;
+    var actGoal = ranges.actGoal;
+    var carbsGoal = ranges.carbsGoal;
+    var medication = ranges.medication;
+    var comment = ranges.comment;
+
     glucMinElement.value = glucMin;
     glucMaxElement.value = glucMax;
     weightMinElement.value = weightMin;
     weightMaxElement.value = weightMax;
+    actGoalElement.value = actGoal;
+    carbsGoalElement.value = carbsGoal;
+    medicationElement.value = medication;
+    commentElement.value = comment;
   });
 }
 
@@ -133,7 +157,11 @@ function saveInput() {
     glucMin: glucMinElement.value,
     glucMax: glucMaxElement.value,
     weightMin: weightMinElement.value,
-    weightMax: weightMaxElement.value
+    weightMax: weightMaxElement.value,
+    actGoal: actGoalElement.value,
+    carbsGoal: carbsGoalElement.value,
+    medication: medicationElement.value,
+    comment: commentElement.value
   }).catch(function (error) {
     console.error('Error writing new message to Firebase Database', error);
   });
