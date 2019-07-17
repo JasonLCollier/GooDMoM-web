94e7a634cd6304e21555dbc1b5a898a654261f8d
Jason <jasoncollier@live.com>
2019-07-17 14:54:35 +0200

    Add event

--- public/scripts/calendar.js
+++ public/scripts/calendar.js
@@ -4,10 +4,21 @@ initFirebaseAuth();
 // initialise calendar
 initCalendar();
 
-// Initialise variables
+// Shortcuts to DOM Elements.
 var signOutElement = document.getElementById('signOut');
+var addEventElement = document.getElementById('addEvent');
+var eventTitleElement = document.getElementById('eventTitle');
+var dateElement = document.getElementById('date');
+var startTimeElement = document.getElementById('startTime');
+var endTimeElement = document.getElementById('endTime');
+var locationElement = document.getElementById('location');
+var patientSelectElement = document.getElementById('patientSelect');
 
+// Initialise variables
 var eventList = new Array();
+var patientList = new Array();
+addEventElement.style.display = "none";
+var calendar = null;
 
 //Sign out click listener
 signOutElement.addEventListener('click', signOut);
@@ -21,8 +32,9 @@ function initFirebaseAuth() {
 // Triggers when the auth state change for instance when the user signs-in or signs-out.
 function authStateObserver(user) {
   if (user) { // User is signed in
-    // load event data
+    // load data
     loadEventData();
+    loadPatientData();
   } else { // User is signed out
     window.open('../index.html', "_self");
   }
@@ -67,7 +79,7 @@ function loadEventData() {
       var userData = data.val();
       patientName = userData.name;
     };
-    patientRef.child("userData").on("value", setName);
+    patientRef.child('userData').on("value", setName);
 
     // Load previous event data and start listening for new data.
     var getData = function (snapshot) {
@@ -87,34 +99,61 @@ function loadEventData() {
   linkedPatientsRef.on('child_changed', loadPatientEvents);
 }
 
+function loadPatientData() {
+  // Read all patients
+  var patientsRef = firebase.database().ref('patients');
+  patientsRef.on("value", function (snapshot) {
+    // For each patient
+    snapshot.forEach(function (data) {
+      // Read the patients user data
+      var ref = patientsRef.child(data.key + "/userData")
+      ref.once("value", function (snap) {
+        var userData = snap.val();
+        var hpNumber = userData.hpNumber;
+        var name = userData.name;
+        // Display the patient if the patient is linked to signed in doctor
+        if (hpNumber == getUserId()) {
+          // patient list with name and id
+          patientList.push([data.key, name]);
+          var option = document.createElement("option");
+          option.text = name;
+          patientSelectElement.add(option);
+        }
+      });
+    });
+  });
+}
+
 function initCalendar() {
   var delayInMilliseconds = 3000;
-
-  var events = new Array();
-
   setTimeout(function () {
     // your code to be executed after x seconds
-    var count = 0;
-    for (i = 0; i < eventList.length; i++) {
-      if (eventList[i][1] == 0) {
-        var startDate = new Date(eventList[i][2]);
-        var endDate = new Date(eventList[i][3]);
-        events[count] = {
-          title: eventList[i][0] + " (" + eventList[i][6] + ")",
-          start: startDate,
-          end: endDate,
-        };
-        count++;
-      }
-    }
-    renderCalendar(events);
+    renderCalendar(populateEvents());
   }, delayInMilliseconds);
+}
+
+function populateEvents() {
+  var events = new Array();
+  var count = 0;
+  for (i = 0; i < eventList.length; i++) {
+    if (eventList[i][1] == 0) {
+      var startDate = new Date(eventList[i][2]);
+      var endDate = new Date(eventList[i][3]);
+      events[count] = {
+        title: eventList[i][0] + " (" + eventList[i][6] + ")",
+        start: startDate,
+        end: endDate,
+      };
+      count++;
+    }
+  }
+  return events;
 }
 
 function renderCalendar(fbEvents) {
   var calendarEl = document.getElementById('calendar');
 
-  var calendar = new FullCalendar.Calendar(calendarEl, {
+  calendar = new FullCalendar.Calendar(calendarEl, {
     plugins: ['interaction', 'dayGrid'],
     header: {
       left: 'prev,next today',
@@ -147,19 +186,46 @@ function renderCalendar(fbEvents) {
       addEventButton: {
         text: 'Add new event',
         click: function () {
-          var dateStr = prompt('Enter date in YYYY-MM-DD format');
-          var date = new Date(dateStr);
-
-          if (date.isValid()) {
-            // Add to firebase
-            // refresh calendar
-          } else {
-            alert('Invalid Date');
-          }
+          addEventElement.style.display = "block";
         }
       }
     },
   });
 
   calendar.render();
+}
+
+function saveInput() {
+  // Add the "show" class to DIV
+  snackbar.className = "show";
+  setTimeout(function () {
+    snackbar.className = snackbar.className.replace("show", "");
+  }, 3000);
+  // Get patient ID
+  var patientIdForList = patientList[patientSelectElement.selectedIndex][0];
+  // Get date
+  var startDate = new Date(dateElement.value + " " + startTimeElement.value);
+  var startDateMillis = startDate.valueOf();
+  var endDate = new Date(dateElement.value + " " + endTimeElement.value);
+  var endDateMillis = endDate.valueOf();
+  // Add a new data entry to the Firebase database.
+  firebase.database().ref('patients/' + patientIdForList + '/events').push({
+    description: "",
+    endDateTime: endDateMillis,
+    eventType: 0,
+    location: locationElement.value,
+    startDateTime: startDateMillis,
+    title: eventTitleElement.value
+  }).catch(function (error) {
+    console.error('Error writing new message to Firebase Database', error);
+  });
+  // Hide dialog
+  addEventElement.style.display = "none";
+  // Reload calendar
+  calendar.destroy();
+  initCalendar();
+}
+
+function cancelInput() {
+  addEventElement.style.display = "none";
 }
